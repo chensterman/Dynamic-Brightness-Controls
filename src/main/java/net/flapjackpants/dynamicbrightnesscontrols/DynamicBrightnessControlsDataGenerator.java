a892e3c8e0b4c1bb521259bed09d71698af66399
Flapjackpants23 <maxkli2007@gmail.com>
2025-03-30 18:07:55 -0400

    test

---
+++ src/main/java/net/flapjackpants/dynamicbrightnesscontrols/DynamicBrightnessControlsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.flapjackpants.dynamicbrightnesscontrols;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DynamicBrightnessControlsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
